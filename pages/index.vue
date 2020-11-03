9ef30589da331872a0624af8e091bedbeb616e2a
Hisashi <hisasi622@gmail.com>
2020-11-03 21:15:08 +0900

    リンク追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,30 @@
 <template lang="pug">
-  h1 別のページに飛んでね
+  .indexTitle
+    h1 下記のリンクから別ページへ
+    .link
+      a(href="airbnb") airbnbページへ
+      a(href="degin") deginページへ
 </template>
 <script>
 export default {}
 </script>
-<style lang="stylus"></style>
+<style lang="stylus">
+.indexTitle
+  width 100%
+  h1
+    text-align center
+.link
+  margin-top 20px
+  display flex
+  justify-content center
+  align-items center
+  a
+    border 1px solid #000
+    width 200px
+    margin-top 20px
+    text-align center
+    margin-left 20px
+    text-decoration none
+    color #ffffff
+    background-color #000
+</style>
